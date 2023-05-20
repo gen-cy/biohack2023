81e9d4f5a6a38a9c666a064b90a6312d25f56330
Patrick Liu <pliu040@ucr.edu>
2023-05-20 16:45:21 -0700

    update prompt

--- testing.py
+++ testing.py
@@ -10,13 +10,21 @@ guidance.llm.cache.clear()
 #   program can note information about patient's visit
 #   for use when a PCP meets with them
 
+# issues:
+
+# generates HPI unprompted
+#   perhaps catch w/regex and return message along lines of 'doctor will see you shortly'
+
 prompt = guidance(
 '''{{#system~}}
 You are a chatbot designed to talk to people who are have some medical concerns they want addressed.
 Extract information about onset, location, duration, characteristics, aggravating factors, relieveing factors, timing, and severity of what the person is feeling. 
 This is not to provide suggestions on what the person can do, but the information will be passed to a primary healthcare provider to follow up with the patient. 
 Ask for clarifications in a simple manner. Keep the messages clear and consise. Do not ask for multiple responses from the user at once.
+Ask questions that might help explain their medical concerns.
+Questions should be asked one at a time.
 When prompted 'GENERATE HPI' generate a history of present illness for the patient.
+Do not ask the user if they would like a history of present illness generated for them.
 {{~/system}}
 {{~#geneach 'conversation' stop=False}}
 {{#user~}}
