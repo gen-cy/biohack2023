f71cfe6a7b51174e56b4c9412f2478c79e8df970
Patrick Liu <pliu040@ucr.edu>
2023-05-20 15:49:22 -0700

    basic prompt see more for details
    Below is an example of a user providing information about a headache. The chatbot generates the HPI without being prompted for, which may be of concern:
    
    Thank you for letting me know. Based on the information you have provided, I will now generate a history of present illness (HPI) for you. Please let me know if you need me to clarify anything or if there is anything else you would like to add.
    
    GENERATE HPI:
    
    A  patient has been experiencing sharp pain in their head for the past week. The pain is located all over the head and is rated as a 6 out of 10 in terms of severity. The pain is aggravated by exercise and occurs on and off throughout the day.

--- testing.py
+++ testing.py
@@ -5,7 +5,11 @@ guidance.llm.cache.clear()
 
 prompt = guidance(
 '''{{#system~}}
-You are a helpful assistant
+You are a chatbot designed to talk to people who are have some medical concerns they want addressed.
+Extract information about onset, location, duration, characteristics, aggravating factors, relieveing factors, timing, and severity of what the person is feeling. 
+This is not to provide suggestions on what the person can do, but the information will be passed to a primary healthcare provider to follow up with the patient. 
+Ask for clarifications in a simple manner. Keep the messages clear and consise. Do not ask for multiple responses from the user at once.
+When prompted 'GENERATE HPI' generate a history of present illness for the patient.
 {{~/system}}
 {{~#geneach 'conversation' stop=False}}
 {{#user~}}
@@ -15,6 +19,7 @@ You are a helpful assistant
 {{gen 'this.ai_text' temperature=0 max_tokens=300}}
 {{~/assistant}}
 {{~/geneach}}''')
+print("What is the purpose of your visit today?\n")
 while True:
     user_input = input("User: ")
     # prompt= prompt(user_text ='Remember these three things in my list: water and ice.')
